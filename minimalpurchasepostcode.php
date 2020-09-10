ffeb184ae8e10940448a140164b3b4081a70d8b7
Mathieu THOLLET <mathieu.thollet@gmail.com>
2020-09-10 11:56:25 +0200

    Bugfix

--- minimalpurchasepostcode.php
+++ minimalpurchasepostcode.php
@@ -39,7 +39,7 @@ class MinimalPurchasePostCode extends Module
     {
         $this->name = 'minimalpurchasepostcode';
         $this->tab = 'checkout';
-        $this->version = '1.0.0';
+        $this->version = '1.0.1';
         $this->author = 'Mathieu Thollet';
         $this->need_instance = 0;
         $this->bootstrap = true;
